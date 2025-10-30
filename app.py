6ff90e197fc3dfdb3149a9fb74ba43613da61929
Vladyslav Yavorskiy <vladyavorskii295@gmail.com>
2025-10-30 12:39:40 +0200

    edited .gitignore

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Головна сторінка - Резюме
 @app.route('/')
-def index():
-    return 'Hello, Flask!'
+def resume():
+    return render_template('resume.html', title='Резюме')
+
+# Сторінка Контакти
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html', title='Контакти')
 
 if __name__ == '__main__':
     app.run(debug=True)
